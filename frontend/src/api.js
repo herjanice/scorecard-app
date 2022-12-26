0b2104e691b3c776856270bb5c0843b34408f7e1
Janice Herman <janice.herman4@gmail.com>
2022-12-26 22:01:10 +0800

    test11

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance = axios.create({
-  baseURL: `http://localhost:4000/`,
-});
+const API_ROOT = "/api"
+const api = axios.create({ baseURL: API_ROOT });
 
-export default instance;
-
-// instance.get('/hi').then((data) => console.log(data));
+export default api
