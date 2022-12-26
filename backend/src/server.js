3153b72d2c24b2e827a795ac8ca17d3e4ef0d49e
Janice Herman <janice.herman4@gmail.com>
2022-12-26 20:50:48 +0800

    test

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+import express, { response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import db from './db';
+import routes from './routes';
+import mongoose from 'mongoose';
+import path from "path";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+app.use(bodyParser.json());
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "../frontend", "build")));
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
+});
+
+db.connect();
+
+const port = process.env.port || 4000
+app.listen(port, () => 
+    console.log(`App listen on port ${port}!`),
+)
