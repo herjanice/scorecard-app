441c75dd4061176e4961bbe033a994e9a1af5ca9
Janice Herman <janice.herman4@gmail.com>
2022-12-27 00:35:04 +0800

    test19

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,18 +7,19 @@ import path from "path";
 
 const app = express();
 
-db.connect();
-
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 app.use('/api', routes);
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "../frontend", "build")));
 app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
 });
+
+db.connect();
 
 const port = process.env.PORT || 4000
 app.listen(port, () => 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_ROOT = "/api"
+const API_ROOT = "https://heroku-scorecard-app.herokuapp.com/api"
 const api = axios.create({ baseURL: API_ROOT });
 
 export default api
