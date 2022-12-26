69aefcc448212c1cad1a9a93ed0364131012d4ba
Janice Herman <janice.herman4@gmail.com>
2022-12-26 22:57:13 +0800

    test15

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,9 +9,7 @@ const app = express();
 
 db.connect();
 
-if (process.env.NODE_ENV === "development") {
-	app.use(cors());
-}
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 app.use(bodyParser.json());
