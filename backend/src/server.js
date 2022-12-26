2fcc29715967ab43d304c4add1a77481d3187d2f
Janice Herman <janice.herman4@gmail.com>
2022-12-26 22:50:30 +0800

    test14

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,16 +9,20 @@ const app = express();
 
 db.connect();
 
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+	app.use(cors());
+}
 app.use(express.json());
 app.use('/api', routes);
 app.use(bodyParser.json());
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "../frontend", "build")));
-app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
-});
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+}
 
 const port = process.env.port || 4000
 app.listen(port, () => 
