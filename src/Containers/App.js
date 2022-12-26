8eb5b8724c0cbd23d6760ecc7e52e8399bffa483
Janice Herman <janice.herman4@gmail.com>
2022-12-26 19:08:21 +0800

    default

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,5 +1,5 @@
-import Header from './containers/Header.js';
-import Body from './containers/Body.js';
+import Header from './Header';
+import Body from './Body';
 import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 
