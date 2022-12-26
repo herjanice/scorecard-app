6546fc1374043ac26a1bc04f91eb0422af63088e
Janice Herman <janice.herman4@gmail.com>
2022-12-26 23:09:01 +0800

    test18

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,13 +14,11 @@ app.use(express.json());
 app.use('/api', routes);
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, "../frontend", "build")));
-    app.get("/*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-    });
-}
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "../frontend", "build")));
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+});
 
 const port = process.env.PORT || 4000
 app.listen(port, () => 
